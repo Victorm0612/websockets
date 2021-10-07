0060997d7b65059e0c19a200c4c7f63e247743ac
victorm0612 <rojasvictorvargas@gmail.com>
2021-10-06 19:09:17 -0500

    simple server setup

--- server-side/index.js
+++ server-side/index.js
@@ -1,32 +1,22 @@
 const express = require("express");
-
 const app = express();
-const httpServer = require("http").createServer(app);
-// settings
-app.set("port", process.env.PORT || 3000);
-
-// start server
-httpServer.listen(app.get("port"), () => {
-  console.log("server on port", app.get("port"));
-});
-
-// Default route to test running server.
-app.get("/", (_, res) => {
-  res.send("App is running");
-});
-
-const socketIO = require("socket.io");
-
-//When server is ready, we pass to socketIO to start the communication channel
-const io = socketIO(httpServer, {
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:4200",
   },
 });
+
+const port = 3000;
+
 io.on("connection", (socket) => {
-  socket.on("addDoc", (data) => {
-    io.sockets.emit("getData", data);
+  console.log("new connection made");
+
+  socket.on("message-received", (data) => {
+    io.sockets.emit("message-sent", data);
   });
 });
 
-module.exports = { io };
+server.listen(port, () => {
+  console.log("Listening on port " + port);
+});
