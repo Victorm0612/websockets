0f982103b5ecdbbbd4de55348a3c7949fd2c4510
victorm0612 <rojasvictorvargas@gmail.com>
2021-10-06 19:04:49 -0500

    added FormsModule and BodyComponent

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { DocumentComponent } from "./document/document.component";
 import { FormsModule } from "@angular/forms";
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
-  declarations: [AppComponent, DocumentComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, BodyComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
