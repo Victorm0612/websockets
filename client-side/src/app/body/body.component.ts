b49f09e85632f91ac913cad0a1a245b7d26a7f31
victorm0612 <rojasvictorvargas@gmail.com>
2021-10-06 19:08:16 -0500

    added custom observer

---
+++ client-side/src/app/body/body.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import * as io from "socket.io-client";
+import { SocketService } from "../socket.service";
+
+@Component({
+  selector: "app-body",
+  templateUrl: "./body.component.html",
+  styleUrls: ["./body.component.scss"],
+})
+export class BodyComponent implements OnInit, OnDestroy {
+  socket: io.Socket;
+  isDisabled: boolean = false;
+  message: string = "";
+
+  constructor(private socketService: SocketService) {
+    //Su
+    this.socketService.$customObservable.subscribe((data) => {
+      this.message = data;
+    });
+  }
+
+  //Disabled button
+  changeState() {
+    this.isDisabled = !this.isDisabled;
+  }
+
+  //Send the message
+  saveMessage() {
+    this.socketService.sentMessage(this.message);
+  }
+
+  //
+  ngOnDestroy() {
+    this.socketService.$customObservable.unsubscribe();
+  }
+
+  ngOnInit() {}
+}
